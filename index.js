a0898ca011d256ae6e9c83255d1c7e15981c9e1d
CodeWithMohaimin <dev.mohaimin@gmail.com>
2023-11-18 18:28:18 +0600

    check repo

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 fetch("https://codewithmohaimin.github.io/creating-serverless-api/data.json")
   .then(function (response) {
-    // The API call was successful!
     return response.json();
   })
   .then(function (data) {
-    // This is the JSON from our response
     console.log(data);
   })
   .catch(function (err) {
-    // There was an error
     console.warn("Something went wrong.", err);
   });
